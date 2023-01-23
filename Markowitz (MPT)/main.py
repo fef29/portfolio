42f326c7acf7b3b8526384b5e129faef009f61c6
fef29 <ferran.ffv@gmail.com>
2023-01-23 01:47:20 +0100

    [NEW] btt and wf finish, cumsum and plot

--- Markowitz (MPT)/main.py
+++ Markowitz (MPT)/main.py
@@ -38,16 +38,61 @@ if __name__ == '__main__':
 
     stocks = ['AAPL', 'AMZN', 'GOOG', 'BRK-B', 'JNJ', 'JPM']
     n = len(stocks)
-    eq_weights = np.array([1/n for i in n])
+    eq_weights = np.array([1/n for i in range(n)])
 
-    start_btt = datetime.date(2022, 1, 1)
+    start = datetime.date(2021, 1, 1)
+    end = start + relativedelta(years=2)
+    daily_dataframes = yf_retrieve_data(stocks, start, end)
+
+    # Backtesting dates
+    start_btt = [start]
+    end_btt = [start + relativedelta(years=1)]
     for i in range(4):
-        start_btt += relativedelta(months=3)
-        end_btt = start_btt + relativedelta(year=1)
+        start_btt.append(start_btt[i] + relativedelta(months=3))
+        end_btt.append(end_btt[i] + relativedelta(months=3))
+
+    # Walk-forward dates
+    start_wf = [start + relativedelta(years=1)]
+    end_wf = [start + relativedelta(years=1, months=3)]
+    for i in range(4):
+        start_wf.append(start_wf[i] + relativedelta(months=3))
+        end_wf.append(end_wf[i] + relativedelta(months=3))
+
+    # weights from backtesting
+    weights_min_var = []
+    weights_risk_tol_100 = []
+    for d1, d2 in zip(start_btt, end_btt):
+        daily_df_split = [aux_df[d1:d2] for aux_df in daily_dataframes]
+        assets = tuple([Asset(name, daily_df) for name, daily_df in zip(stocks, daily_df_split)])
+        portfolio = Portfolio(assets)
+        # weights minimum variance portfolio
+        portfolio.optimize_with_risk_tolerance(0)
+        weights_min_var.append(portfolio.weights)
+        # weights risk tolerance portfolio
+        portfolio.optimize_with_risk_tolerance(100)
+        weights_risk_tol_100.append(portfolio.weights)
 
 
+    # Walkforward
+    returns_min = []
+    returns_risk = []
+    returns_equal = []
+    for d1, d2, w_m, w_r in zip(start_wf, end_wf, weights_min_var, weights_risk_tol_100):
+        # asset returns
+        daily_df_split = [Asset.get_log_period_returns(aux_df[d1:d2]) for aux_df in daily_dataframes]
 
-    daily_dataframes = yf_retrieve_data(stocks, start_btt, end_btt)
+        returns_m = [asset_returns * _w for asset_returns, _w in zip(daily_df_split, w_m)]
+        returns_min.append(sum(returns_m))
+
+        returns_r = [asset_returns * _w for asset_returns, _w in zip(daily_df_split, w_r)]
+        returns_risk.append(sum(returns_r))
+
+        returns_eq = [asset_returns * _w for asset_returns, _w in zip(daily_df_split, eq_weights)]
+        returns_equal.append(sum(returns_eq))
+
+    # cumsum and plot
+
+    # daily_dataframes = yf_retrieve_data(stocks, start_btt, end_btt)
     assets = tuple([Asset(name, daily_df) for name, daily_df in zip(stocks, daily_dataframes)])
     portfolio = Portfolio(assets)
 
